2f519c732e87185a0714a7e2477fbf07eee5d328
Neus <neuscalvo11@gmail.com>
2021-03-19 12:42:19 +0100

    Adding switch case

--- ex/app1.js
+++ ex/app1.js
@@ -1,20 +1,32 @@
+const { copyFileSync } = require('node:fs');
+
 // Create an instance of readline by configuring the readable and the writable streams
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
-  });
-
-
-
-
+    });           
   
-  readline.question('Welcome, please enter your username?', function(userName){
+  readline.question('Welcome, please enter your username? ', function(userName){
        console.log(`Hey there ${userName}!`);
        menuTasks();
-          readline.question('Which action are you intending to perform?', function(action){
-          console.log(action)
+          readline.question('Which action are you intending to perform?  ', function(action){
+          switch(action) {
+                case "1":
+                    console.log('New task');
+                    break;
+                case "2":
+                    console.log('Edit task');
+                    break;
+                case "3": 
+                    console.log('Delete task');
+                    break;
+                case "4":
+                    console.log('Show tasks');
+                default:
+                    console.log('See you soon!!');
+          }
        readline.close();
-});
+    });
 });
     
 // Show Menu-options

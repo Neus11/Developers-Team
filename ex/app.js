b70377a33075ff0f3a0f95afd203557c222ec4a8
Ismael Nieto <ismael.nieto@gmail.com>
2021-03-19 12:54:18 +0100

    Recursive function added.

--- ex/app.js
+++ ex/app.js
@@ -1,24 +1,41 @@
 // Create an instance of readline by configuring the readable and the writable streams
-const readline = require('readline').createInterface({
+var readline = require('readline');
+
+var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-  });
-   
-  readline.question('Welcome, please enter your username:  ', userName => {
-    console.log(`Hey there ${userName}!`);
-    menuTasks();
-    readline.close();
-  });
-// Show Menu-options
-function menuTasks(){
+});
 
-    console.log("Menu:");
-    console.log("1. Add new task.");
-    console.log("2. Edit an existing task.");
-    console.log("3. Delete task.");
-    console.log("4. Show all tasks.");
-    console.log("5. Exit.")
-    
+var recursiveAsyncReadLine = function () {
+    rl.question("Please Choose an option:\n"
+        + "1) Add new task\n"
+        + "2) Edit an existing task\n"
+        + "3) Delete a task \n"
+        + "4) Show all tasks\n"
+        + "5) Exit app\n"
+        , function (line) {
 
-}
+            switch (line){
+                case "1":
+                   console.log("this is option 1");
+                   break;
+                case "2":
+                   console.log("this is option 2");
+                   break;
+               case "3":
+                   console.log("this is option 3");
+                   break;
+               case "4":
+                   console.log("this is option 4");
+                   break;
+                case "5":
+                    return rl.close();
+                    break;
+                default:
+                    console.log("No such option. Please enter another: ");
+            }
+    recursiveAsyncReadLine(); //Calling this function again to ask new question
+    });
+};
 
+recursiveAsyncReadLine();

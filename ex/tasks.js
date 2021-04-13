466f60bdd95066de68c3f1e0569d9ae9d073c56a
Ismael Nieto <ismael.nieto@gmail.com>
2021-04-13 13:35:16 +0200

    Errors found in delete and path to file JSON

--- ex/tasks.js
+++ ex/tasks.js
@@ -33,7 +33,7 @@ const TaskFunctions = (function( ) {
     return {
         getTasklist: ()=>{
             // read list of tasks from file
-            let tasks = fs.readFileSync('./TASQUES.json');
+            let tasks = fs.readFileSync('TASQUES.json');
             // Parse JSON  constructing the JavaScript value or object described by the JSON - string. 
             let taskList = JSON.parse(tasks); 
             // Print on console only the list of tasks with a number
@@ -42,11 +42,8 @@ const TaskFunctions = (function( ) {
         addNewTask: function(newTask, taskList) {
             return new Promise((resolve, reject) => {
             // Create a new Instance of class Task
-            /* let t = taskMaker.createTask(...newTask);
-            // Convert instance Task to a plain object or not possible to  be added to the collecion of Tasks
             let t2 = Object.assign({}, t);
             taskList.push(t2); // Add new task to collection
-             */
             taskList.push(taskMaker.createTask(...newTask));
             fs.writeFileSync('TASQUES.json', JSON.stringify(taskList), {flag: "w+"});
             console.log("Task was succesfully added!")  
@@ -56,15 +53,8 @@ const TaskFunctions = (function( ) {
         deleteTask : function(input, arrTask) {
             let selectedTask = parseInt(input);
             selectedTaskIndex = selectedTask -1; //encontrar index del elemento seleccionado
-           //getTasklist().splice(selectedTaskIndex, 1);
-            let counter = 0;
-            const tr = arrTask.filter((item) => {
-                    if (selectedTaskIndex !== counter){
-                        return item;
-                    };
-                     counter++;
-                });
-            fs.writeFileSync('./TASQUES.json', JSON.stringify(tr), {flag: "w+"});
+            arrTask.splice(selectedTaskIndex, 1);
+            fs.writeFileSync('./TASQUES.json', JSON.stringify(arrTask), {flag: "w+"});
             console.log("Task was succesfully deleted!")  
         },
         viewTask: function(input, arrTask) {
